c859bd191d2683b6e2b722f59a9cf61c92b1e931
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-03-25 19:52:38 +0100

    added post http request in login page, now just a dummy, to be tested, added http lib to project

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import './../SimpleTextField.dart';
+import 'package:http/http.dart' as http;
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+
+  String urlStr = "http://ratti.dynv6.net/appartapp_war_exploded/api/login";
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  String status="Not submitted yet";
+
   @override
   Widget build(BuildContext context) {
     Color bgColor = Colors.white;
@@ -9,11 +22,11 @@ class Login extends StatelessWidget {
     return Scaffold(
         backgroundColor: bgColor,
         body: SafeArea(
-        child: Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          /*
+            child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      /*
           const SimpleTextField(
             labelText: 'E-Mail',
             showLabelAboveTextField: true,
@@ -28,36 +41,60 @@ class Login extends StatelessWidget {
           ),
 
            */
-        Padding(
-        padding: EdgeInsets.all(16.0),
-            child: Text(
-              "Inserisci le tue credenziali",
-              style: TextStyle(fontSize: 20),
-            )),
-          const Padding(
-              padding: EdgeInsets.all(16.0),
-        child:
-          const TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'E-Mail',
-            )
-          )),
-          const Padding(
-              padding: EdgeInsets.all(16.0),
-    child:
-              const TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
-            ),
-          )),
-          ElevatedButton(
-            child:Text("Accedi"),
-            onPressed: () {},
-          ),
-          ]))));
+                      Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            "Inserisci le tue credenziali",
+                            style: TextStyle(fontSize: 20),
+                          )),
+                      Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            status,
+                            style: TextStyle(fontSize: 20),
+                          )),
+                      Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: TextField(
+                            obscureText: false,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'E-Mail',
+                            ),
+                            controller: emailController,
+                          )),
+                      Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: TextField(
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Password',
+                            ),
+                          )),
+                      ElevatedButton(
+                        child: Text("Accedi"),
+                        onPressed: () {
+                          String email = emailController.text;
+                          String password = passwordController.text;
+
+                          var url = Uri.parse(widget.urlStr);
+                          http.post(
+                              url, body: {'email': email, 'password': password}).then((response) => setState(() {
+                              if (response.statusCode!=200)
+                                status="Failure";
+                              else
+                                status="Logged in"+response.body;
+                          }));
+                        },
+                      ),
+                    ]))));
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
   }
 }

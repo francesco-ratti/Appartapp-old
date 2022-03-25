19f5937257e02183d848dd02b9ac16ceaa23e3f9
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-03-25 21:53:53 +0100

    added post http request in login page, now just a dummy, to be tested, added http lib to project

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,10 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import './../SimpleTextField.dart';
 import 'package:http/http.dart' as http;
 
 class Login extends StatefulWidget {
+  String urlStr = "http://ratti.dynv6.net/appartapp-1.0-SNAPSHOT/api/login";
 
-  String urlStr = "http://ratti.dynv6.net/appartapp_war_exploded/api/login";
   @override
   _LoginState createState() => _LoginState();
 }
@@ -13,7 +15,7 @@ class _LoginState extends State<Login> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
-  String status="Not submitted yet";
+  String status = "Not submitted yet";
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +28,7 @@ class _LoginState extends State<Login> {
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      /*
+              /*
           const SimpleTextField(
             labelText: 'E-Mail',
             showLabelAboveTextField: true,
@@ -41,55 +43,65 @@ class _LoginState extends State<Login> {
           ),
 
            */
-                      Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: Text(
-                            "Inserisci le tue credenziali",
-                            style: TextStyle(fontSize: 20),
-                          )),
-                      Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: Text(
-                            status,
-                            style: TextStyle(fontSize: 20),
-                          )),
-                      Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: TextField(
-                            obscureText: false,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'E-Mail',
-                            ),
-                            controller: emailController,
-                          )),
-                      Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: TextField(
-                            controller: passwordController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Password',
-                            ),
-                          )),
-                      ElevatedButton(
-                        child: Text("Accedi"),
-                        onPressed: () {
-                          String email = emailController.text;
-                          String password = passwordController.text;
+              Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    "Inserisci le tue credenziali",
+                    style: TextStyle(fontSize: 20),
+                  )),
+              Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    status,
+                    style: TextStyle(fontSize: 20),
+                  )),
+              Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: TextField(
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'E-Mail',
+                    ),
+                    controller: emailController,
+                  )),
+              Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  )),
+              ElevatedButton(
+                child: Text("Accedi"),
+                onPressed: () {
+                  const HtmlEscape htmlEscape = HtmlEscape();
 
-                          var url = Uri.parse(widget.urlStr);
-                          http.post(
-                              url, body: {'email': email, 'password': password}).then((response) => setState(() {
-                              if (response.statusCode!=200)
-                                status="Failure";
-                              else
-                                status="Logged in"+response.body;
+                  String email = htmlEscape.convert(emailController.text);
+                  String password = htmlEscape.convert(passwordController.text);
+
+                  var url = Uri.parse(widget.urlStr);
+                  var body = utf8.encode("email=${email}&password=${password}");
+                  http
+                      .post(url,
+                          headers: {
+                            "Content-Length": body.length.toString(),
+                            "Content-Type": "application/x-www-form-urlencoded"
+                          },
+                          encoding: Encoding.getByName('utf-8'),
+                          body: body)
+                      .then((response) => setState(() {
+                            if (response.statusCode != 200)
+                              status = "Failure" + response.body;
+                            else
+                              status = "Logged in" + response.body;
                           }));
-                        },
-                      ),
-                    ]))));
+                },
+              ),
+            ]))));
   }
 
   @override

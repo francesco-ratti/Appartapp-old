2f7b95acfa13b96bb342b3a812e9b5ff7adff53a
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-03-26 13:07:51 +0100

    login refactoring, now widget is a materialapp with an appBar

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -13,7 +13,6 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-
   void doLogin(Function(String) updateUi, String email, String password) async {
     var dio = Dio();
     try {
@@ -22,9 +21,7 @@ class _LoginState extends State<Login> {
         data: {"email": email, "password": password},
         options: Options(
           contentType: Headers.formUrlEncodedContentType,
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-          },
+          headers: {"Content-Type": "application/x-www-form-urlencoded"},
         ),
       );
 
@@ -48,14 +45,15 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     Color bgColor = Colors.white;
 
-    return Scaffold(
-        backgroundColor: bgColor,
-        body: SafeArea(
-            child: Center(
+    return (MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: const Text('Login')),
+            backgroundColor: bgColor,
+            body: Center(
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-              /*
+                  /*
           const SimpleTextField(
             labelText: 'E-Mail',
             showLabelAboveTextField: true,
@@ -70,75 +68,51 @@ class _LoginState extends State<Login> {
           ),
 
            */
-              Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    "Inserisci le tue credenziali",
-                    style: TextStyle(fontSize: 20),
-                  )),
-              Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    status,
-                    style: TextStyle(fontSize: 20),
-                  )),
-              Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: TextField(
-                    obscureText: false,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'E-Mail',
-                    ),
-                    controller: emailController,
-                  )),
-              Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: TextField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    ),
-                  )),
-              ElevatedButton(
-                  child: Text("Accedi"),
-                  onPressed: () {
-                    /*
-                  const HtmlEscape htmlEscape = HtmlEscape();
+                  Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        "Inserisci le tue credenziali",
+                        style: TextStyle(fontSize: 20),
+                      )),
+                  Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        status,
+                        style: TextStyle(fontSize: 20),
+                      )),
+                  Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: TextField(
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'E-Mail',
+                        ),
+                        controller: emailController,
+                      )),
+                  Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: TextField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                        ),
+                      )),
+                  ElevatedButton(
+                      child: Text("Accedi"),
+                      onPressed: () {
+                        String email = emailController.text;
+                        String password = passwordController.text;
 
-                  String email = htmlEscape.convert(emailController.text);
-                  String password = htmlEscape.convert(passwordController.text);
-
-                  var url = Uri.parse(widget.urlStr);
-                  var body = utf8.encode("email=${email}&password=${password}");
-                  http
-                      .post(url,
-                          headers: {
-                            "Content-Length": body.length.toString(),
-                            "Content-Type": "application/x-www-form-urlencoded"
-                          },
-                          encoding: Encoding.getByName('utf-8'),
-                          body: body)
-                      .then((response) => setState(() {
-                            if (response.statusCode != 200)
-                              status = "Failure" + response.body;
-                            else
-                              status = "Logged in" + response.body;
-                          }));*/
-                    //Instance level
-                    String email = emailController.text;
-                    String password = passwordController.text;
-
-                    doLogin((String toWrite) {
-                      setState(() {
-                        status=toWrite;
-                      });
-                    }, email, password);
-
-                  })
-            ]))));
+                        doLogin((String toWrite) {
+                          setState(() {
+                            status = toWrite;
+                          });
+                        }, email, password);
+                      })
+                ])))));
   }
 
   @override

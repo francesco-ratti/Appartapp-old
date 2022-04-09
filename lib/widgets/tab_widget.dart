81a74d4296fbfb357c3a88f6c78437996396008a
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-04-09 15:14:34 +0200

    parametrised tabWidget costructor in order to instantiate it from parent widget with required properties

--- lib/widgets/tab_widget.dart
+++ lib/widgets/tab_widget.dart
@@ -4,34 +4,38 @@ class TabWidget extends StatelessWidget {
   TabWidget({
     Key? key,
     required this.scrollController,
+    required this.listingTitle,
+    required this.description,
+    required this.price,
+    required this.address,
+    required this.additionalExpenseDetail
   }) : super(key: key);
   final ScrollController scrollController;
-
-  String listingTitle="Bilocale Milano";
-  String description="Casa molto carina, senza soffitto, senza cucina";
-  int price=350;
-  String address="Via Roma, 27";
-  String additionalExpenseDetail="No, pagamento trimestrale";
+  final String listingTitle;//="Bilocale Milano";
+  final String description;//="Casa molto carina, senza soffitto, senza cucina";
+  final int price;//=350;
+  final String address;//="Via Roma, 27";
+  final String additionalExpenseDetail;//="No, pagamento trimestrale";
 
   @override
   Widget build(BuildContext context) => ListView(
-        padding: EdgeInsets.all(16),
-        controller: scrollController,
-        children: [
-          Text(
-            listingTitle,
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20),
-          ),
-          Container(
-            height: 300,
-            width: 300,
-            child: Image.asset('assets/appart.tiff'),
-          ),
-          Text(description),
-          Text("Prezzo: ${price}€"),
-          Text("Indirizzo: ${address}"),
-          Text("Spese incluse: ${additionalExpenseDetail}"),
-        ],
-      );
+    padding: EdgeInsets.all(16),
+    controller: scrollController,
+    children: [
+      Text(
+        listingTitle,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 20),
+      ),
+      Container(
+        height: 300,
+        width: 300,
+        child: Image.asset('assets/appart.tiff'),
+      ),
+      Text(description),
+      Text("Prezzo: ${price}€"),
+      Text("Indirizzo: ${address}"),
+      Text("Spese incluse: ${additionalExpenseDetail}"),
+    ],
+  );
 }

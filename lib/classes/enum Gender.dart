170a309140654c63e7de17e0e3bb0f735b242769
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-05-03 01:00:45 +0200

    added edit_profile page (and enum Gender that I missed uploading in the prev commit; associated with User)

---
+++ lib/classes/enum Gender.dart
@@ -0,0 +1,5 @@
+enum Gender {
+  M,
+  F,
+  NB
+}
---
+++ lib/pages/edit_profile.dart
@@ -0,0 +1,248 @@
+import 'package:appartapp/classes/User.dart';
+import 'package:appartapp/classes/enum%20Gender.dart';
+import 'package:flutter/material.dart';
+
+//import './../SimpleTextField.dart';
+import 'package:dio/dio.dart';
+
+import '../classes/runtime_store.dart';
+
+class EditProfile extends StatefulWidget {
+  String urlStr = "http://ratti.dynv6.net/appartapp-1.0-SNAPSHOT/api/signup";
+
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
+class GenderForList {
+  const GenderForList(this.name, this.code);
+
+  final String name;
+  final String code;
+}
+class _EditProfileState extends State<EditProfile> {
+  void doSignup(Function(String) updateUi, String email, String password,
+      String name, String surname, DateTime birthday, GenderForList gender) async {
+    var dio = Dio();
+    try {
+      Response response = await dio.post(
+        widget.urlStr,
+        data: {
+          "email": email,
+          "password": password,
+          "name": name,
+          "surname": surname,
+          "birthday": birthday.millisecondsSinceEpoch.toString(),
+          "gender": gender.code
+        },
+        options: Options(
+          contentType: Headers.formUrlEncodedContentType,
+          headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        ),
+      );
+
+      if (response.statusCode != 200)
+        updateUi("Failure");
+      else
+        updateUi("Signed up");
+    } on DioError catch (e) {
+      if (e.response?.statusCode != 200) {
+        updateUi("Failure");
+      }
+    }
+  }
+
+  final emailController = TextEditingController();
+  //final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final surnameController = TextEditingController();
+  final birthdayController = TextEditingController();
+
+  String status = "Not submitted yet";
+
+  User? user=RuntimeStore().getUser();
+
+  late DateTime _birthday=new DateTime.now();
+
+  GenderForList genderM=GenderForList("Maschile", "M");
+  GenderForList genderF=GenderForList("Femminile", "F");
+  GenderForList genderNB=GenderForList("Non binario", "NB");
+
+  late List<GenderForList> _genders; // Option 2
+  var _selectedGender=null; // Option 2
+
+  @override
+  void initState() {
+    super.initState();
+
+    _genders=<GenderForList>[
+      genderM,
+      genderF,
+      genderNB
+    ];
+
+    _birthday = user!.birthday;
+
+    switch (user!.gender) {
+      case Gender.M:
+        _selectedGender=genderM;
+        break;
+      case Gender.F:
+        _selectedGender=genderM;
+        break;
+      case Gender.NB:
+        _selectedGender=genderNB;
+        break;
+    }
+
+    emailController.text=user!.email;
+    nameController.text=user!.name;
+    surnameController.text=user!.surname;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color bgColor = Colors.white;
+
+    birthdayController.text =
+    "${_birthday.day}/${_birthday.month}/${_birthday.year}";
+
+    return Scaffold(
+        appBar: AppBar(title: const Text('Il tuo profilo')),
+        backgroundColor: bgColor,
+        body: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'E-Mail',
+                  ),
+                  controller: emailController,
+                )),
+            /*Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                      controller: passwordController,
+                    )),
+
+                 */
+            Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Nome',
+                  ),
+                  controller: nameController,
+                )),
+            Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Cognome',
+                  ),
+                  controller: surnameController,
+                )),
+            Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: birthdayController,
+                  readOnly: true,
+                  onTap: () {
+                    showDatePicker(
+                        context: context,
+                        initialDate: _birthday,
+                        lastDate: DateTime.now(),
+                        firstDate: DateTime(1900))
+                        .then((value) {
+                      if (value != null) {
+                        setState(() {
+                          _birthday = value;
+                        });
+                      }
+                    });
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Data di nascita',
+                  ),
+                )),
+            Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(children: [
+                  Expanded(
+                      child: DropdownButton(
+                        hint: Text("Scegli il tuo genere"),
+                        // Not necessary for Option 1
+                        value: _selectedGender,
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedGender = newValue;
+                          });
+                        },
+                        items: _genders.map((gender) {
+                          return DropdownMenuItem(
+                            child: new Text(gender.name),
+                            value: gender,
+                          );
+                        }).toList(),
+                      ))
+                ])),
+            ElevatedButton(
+                child: Text("Registrati"),
+                onPressed: () {
+                  String email = emailController.text;
+                  //String password = passwordController.text;
+                  String name = nameController.text;
+                  String surname = surnameController.text;
+
+                  if ((email.length > 0 &&
+                      //password.length > 0 &&
+                      name.length > 0 &&
+                      surname.length > 0 &&
+                      _selectedGender != null)) {
+
+                    /*doSignup((String toWrite) {
+                          setState(() {
+                            status = toWrite;
+                          });
+                        }, email, password, name, surname, _birthday,
+                            _selectedGender);
+
+                         */
+//TODO upload modifications
+
+                  } else {
+                    setState(() {
+                      status = "Incompleto. Compila tutti i campi";
+                    });
+                  }
+                }),
+            Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  status,
+                  style: TextStyle(fontSize: 20),
+                )),
+          ],
+        ));
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    nameController.dispose();
+    surnameController.dispose();
+    birthdayController.dispose();
+    //passwordController.dispose();
+  }
+}

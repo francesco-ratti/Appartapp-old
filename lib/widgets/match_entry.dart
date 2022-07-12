9201df5a0b132ea36165d2e48a3e85d1c35a1e78
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-07-12 03:50:50 +0200

    improvements to time interval textual representation in match entry

--- lib/widgets/match_entry.dart
+++ lib/widgets/match_entry.dart
@@ -8,6 +8,52 @@ class MatchEntry extends StatelessWidget {
   const MatchEntry({Key? key, required this.match, required this.onTileTap})
       : super(key: key);
 
+  String timeDifferenceBuilder(Duration duration) {
+    int minutes = duration.inMinutes;
+    if (minutes == 0) {
+      return "ora";
+    }
+    if (minutes == 1) {
+      return "un minuto fa";
+    }
+    if (minutes < 60) {
+      return "${minutes} minuti fa";
+    }
+    int hours = duration.inHours;
+    if (hours == 1) {
+      return "un'ora fa";
+    }
+    if (hours < 24) {
+      return "${hours} ore fa";
+    }
+    int days = duration.inDays;
+    if (days == 1) {
+      return "un giorno fa";
+    }
+    if (days < 7 * 4) {
+      return "${days} giorni fa";
+    }
+    int weeks = days ~/ 7;
+    /*if (weeks==1) {
+      return "una settimana fa";
+    }*/
+    if (weeks < 4 * 4) {
+      return "${weeks} settimane fa";
+    }
+    int months = weeks ~/ 4;
+    /*if (months==1) {
+      return "un mese fa";
+    }*/
+    if (months < 12 * 2) {
+      return "${months} mesi fa";
+    }
+    int years = months ~/ 12;
+    /*if (years==1) {
+      return "un anno fa";
+    }*/
+    return "${years} anni fa";
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -18,7 +64,7 @@ class MatchEntry extends StatelessWidget {
             ),
       title: Text(match.apartment.listingTitle),
       subtitle: Text(
-          "${match.apartment.description} - ${DateTime.now().difference(match.time).inMinutes} minuti fa"),
+          "${match.apartment.description} - ${timeDifferenceBuilder(DateTime.now().difference(match.time))}"),
       onTap: () {
         onTileTap(context, match);
 

2a56d635f8bea28cd47f64d243296dd3be679b7b
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-07-13 20:17:21 +0200

    error management (cannot open email client/no email client installed) and ui improvements to contact apartment page

--- lib/widgets/contact_apartment.dart
+++ lib/widgets/contact_apartment.dart
@@ -1,5 +1,7 @@
 import 'package:appartapp/classes/apartment.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 class ContactApartment extends StatelessWidget {
@@ -8,6 +10,48 @@ class ContactApartment extends StatelessWidget {
   final Color textColor;
   final Color? backgroundColor;
 
+  static Route<Object?> buildNoEmailClientErrorRoute(
+      BuildContext context, Object? arguments) {
+    return CupertinoDialogRoute<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+            title: const Text("Errore"),
+            actions: [
+              CupertinoDialogAction(
+                  child: Text("OK"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  })
+            ],
+            content: Center(
+              child: Text("Nessun client di posta installato"),
+            ));
+      },
+    );
+  }
+
+  static Route<Object?> buildEmailErrorRoute(
+      BuildContext context, Object? arguments) {
+    return CupertinoDialogRoute<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+            title: const Text("Errore"),
+            actions: [
+              CupertinoDialogAction(
+                  child: Text("OK"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  })
+            ],
+            content: Center(
+              child: Text("Impossibile aprire il client di posta"),
+            ));
+      },
+    );
+  }
+
   const ContactApartment(
       {Key? key,
       required this.textColor,
@@ -15,43 +59,55 @@ class ContactApartment extends StatelessWidget {
       required this.apartment})
       : super(key: key);
 
-  Widget buildBody() {
+  Widget buildBody(BuildContext context) {
     return Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
           Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 8.0),
+              padding: const EdgeInsets.all(8.0),
               child: Text(
                 "Contatta ${apartment.owner!.name} ${apartment.owner!.surname} per email:",
-                style: TextStyle(color: textColor),
+                style: TextStyle(color: textColor, fontSize: 15),
                 textAlign: TextAlign.center,
               )),
           Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 8.0),
+              padding: const EdgeInsets.all(8.0),
               child: Text(
                 apartment.owner!.email,
                 style: TextStyle(color: textColor, fontSize: 20),
                 textAlign: TextAlign.center,
               )),
-          ElevatedButton(
-              child: const Text("Invia un'email"),
-              style: ElevatedButton.styleFrom(primary: Colors.brown),
-              onPressed: () async {
-                final Email email = Email(
-                  body:
-                      "Ciao ${apartment.owner!.name}, ti contatto per l'appartamento ${apartment.description} in ${apartment.address}.\n",
-                  subject: "Annuncio ${apartment.description}",
-                  recipients: [apartment.owner!.email],
-                  //cc: ['cc@example.com'],
-                  //bcc: ['bcc@example.com'],
-                  //attachmentPaths: ['/path/to/attachment.zip'],
-                  isHTML: false,
-                );
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                  child: const Text("Scrivi un'email"),
+                  style: ElevatedButton.styleFrom(primary: Colors.brown),
+                  onPressed: () async {
+                    try {
+                      final Email email = Email(
+                        body:
+                            "Ciao ${apartment.owner!.name}, ti contatto per l'appartamento ${apartment.description} in ${apartment.address}.\n",
+                        subject: "Annuncio ${apartment.description}",
+                        recipients: [apartment.owner!.email],
+                        //cc: ['cc@example.com'],
+                        //bcc: ['bcc@example.com'],
+                        //attachmentPaths: ['/path/to/attachment.zip'],
+                        isHTML: false,
+                      );
 
-                await FlutterEmailSender.send(email);
-              })
+                      await FlutterEmailSender.send(email);
+                    } on PlatformException catch (e) {
+                      if (e.code == "not_available") {
+                        Navigator.restorablePush(
+                            context, buildNoEmailClientErrorRoute);
+                      } else {
+                        Navigator.restorablePush(
+                            context, buildNoEmailClientErrorRoute);
+                      }
+                    }
+                  })),
         ]));
   }
 
@@ -63,13 +119,13 @@ class ContactApartment extends StatelessWidget {
               title: Text("Contatta ${apartment.owner?.name}"),
               backgroundColor: Colors.brown,
             ),
-            body: buildBody())
+            body: buildBody(context))
         : Scaffold(
             backgroundColor: backgroundColor,
             appBar: AppBar(
               title: Text("Contatta ${apartment.owner?.name}"),
               backgroundColor: Colors.brown,
             ),
-            body: buildBody());
+            body: buildBody(context));
   }
 }

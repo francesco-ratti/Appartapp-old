0e6c3ef2d607f7753510d19082b68e56d796f2bf
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-07-14 16:11:27 +0200

    retry widget ui improvements and add_apartment_informations.dart removed (useless, retry widget used instead)

--- lib/widgets/retry_widget.dart
+++ lib/widgets/retry_widget.dart
@@ -20,23 +20,25 @@ class RetryWidget extends StatelessWidget {
       : super(key: key);
 
   Widget buildBody() {
-    return Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-          Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 8.0),
-              child: Text(
-                message,
-                style: TextStyle(color: textColor),
-                textAlign: TextAlign.center,
-              )),
-          ElevatedButton(
-              child: Text(retryButtonText),
-              style: ElevatedButton.styleFrom(primary: Colors.brown),
-              onPressed: retryCallback)
-        ]));
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 0),
+        child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 8.0),
+                  child: Text(
+                    message,
+                    style: TextStyle(color: textColor),
+                    textAlign: TextAlign.center,
+                  )),
+              ElevatedButton(
+                  child: Text(retryButtonText),
+                  style: ElevatedButton.styleFrom(primary: Colors.brown),
+                  onPressed: retryCallback)
+            ])));
   }
 
   @override

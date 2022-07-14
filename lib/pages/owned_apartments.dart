acca3fd3954d16b681c7edd06cc7d88c12d78766
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-07-14 16:10:39 +0200

    loading indicator for owned_apartments.dart (if update isn't running in background, only if no info at all about owned apartments)

--- lib/pages/owned_apartments.dart
+++ lib/pages/owned_apartments.dart
@@ -6,6 +6,7 @@ import 'package:appartapp/pages/add_apartment.dart';
 import 'package:appartapp/widgets/apartment_viewer.dart';
 import 'package:appartapp/widgets/retry_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class OwnedApartments extends StatefulWidget {
   const OwnedApartments({Key? key}) : super(key: key);
@@ -18,14 +19,29 @@ class _OwnedApartments extends State<OwnedApartments> {
   void updateUi(List<Apartment> value) {
     setState(() {
       ownedApartments = value;
+      if (_isLoading) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
     });
   }
 
   List<Apartment>? ownedApartments = null;
   bool _networkError = false;
+  bool _isLoading = false;
 
-  void updateApartments() {
-    _networkError = false;
+  void updateApartments({bool showLoading = false}) {
+    if (_networkError) {
+      setState(() {
+        _networkError = false;
+      });
+    }
+    if (showLoading) {
+      setState(() {
+        _isLoading = true;
+      });
+    }
     Future<List<Apartment>> newOwnedApartments =
         ApartmentHandler().getOwnedApartments();
     newOwnedApartments
@@ -47,6 +63,8 @@ class _OwnedApartments extends State<OwnedApartments> {
 
   @override
   void initState() {
+    _isLoading = false;
+
     Future<List<Apartment>>? oldOwnedApartments =
         RuntimeStore().getOwnedApartments();
 
@@ -69,100 +87,109 @@ class _OwnedApartments extends State<OwnedApartments> {
           settings: settings,
           builder: (BuildContext context) {
             return Scaffold(
-              floatingActionButton: FloatingActionButton(
-                heroTag: null,
-                backgroundColor: Colors.brown,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const AddApartment()),
-                  );
-                },
-                child: const Icon(Icons.add),
-              ),
-              body: ownedApartments == null
-                  ? Center(
-                      child: _networkError
-                          ? RetryWidget(
-                              textColor: Colors.black,
-                              retryCallback: updateApartments)
-                          : const CircularProgressIndicator(
-                              value: null,
-                            ))
-                  : ownedApartments!.isEmpty
-                      ? const Center(
-                          child: Text(
-                              "Non possiedi alcun appartamento. Aggiungine uno!"),
-                        )
-                      : ListView.builder(
-                          itemCount: ownedApartments!.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            Apartment currentApartment =
-                                ownedApartments![index];
+                floatingActionButton: FloatingActionButton(
+                  heroTag: null,
+                  backgroundColor: Colors.brown,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AddApartment()),
+                    );
+                  },
+                  child: const Icon(Icons.add),
+                ),
+                body: ModalProgressHUD(
+                  child: ownedApartments == null
+                      ? Center(
+                          child: _networkError
+                              ? RetryWidget(
+                                  textColor: Colors.black,
+                                  retryCallback: () =>
+                                      updateApartments(showLoading: true))
+                              : const CircularProgressIndicator(
+                                  value: null,
+                                ))
+                      : ownedApartments!.isEmpty
+                          ? const Center(
+                              child: Text(
+                                  "Non possiedi alcun appartamento. Aggiungine uno!"),
+                            )
+                          : ListView.builder(
+                              itemCount: ownedApartments!.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                Apartment currentApartment =
+                                    ownedApartments![index];
 
-                            return ListTile(
-                              leading: currentApartment.images[0] != null
-                                  ? CircleAvatar(
-                                      backgroundImage:
-                                          currentApartment.images[0].image)
-                                  : const Icon(
-                                      Icons.apartment_rounded,
-                                    ),
-                              title: Text(currentApartment.listingTitle),
-                              subtitle: Text(currentApartment.description),
-                              onTap: () {
-                                for (final Image im
-                                    in ownedApartments![index].images) {
-                                  precacheImage(im.image, context);
-                                }
+                                return ListTile(
+                                  leading: currentApartment.images[0] != null
+                                      ? CircleAvatar(
+                                          backgroundImage:
+                                              currentApartment.images[0].image)
+                                      : const Icon(
+                                          Icons.apartment_rounded,
+                                        ),
+                                  title: Text(currentApartment.listingTitle),
+                                  subtitle: Text(currentApartment.description),
+                                  onTap: () {
+                                    for (final Image im
+                                        in ownedApartments![index].images) {
+                                      precacheImage(im.image, context);
+                                    }
 
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => Scaffold(
-                                            appBar: AppBar(
-                                                title: Text(
-                                                    ownedApartments![index]
-                                                        .listingTitle),
-                                                backgroundColor: Colors.brown,
-                                                actions: <Widget>[
-                                                  Padding(
-                                                      padding:
-                                                      const EdgeInsets.only(
-                                                          right: 20.0),
-                                                      child: GestureDetector(
-                                                        onTap: () {
-                                                          Navigator.push(
-                                                            context,
-                                                            MaterialPageRoute(
-                                                                builder:
-                                                                    (context) =>
-                                                                        AddApartment(
-                                                                          toEdit:
-                                                                              ownedApartments![index],
-                                                                        )),
-                                                          );
-                                                        },
-                                                        child: const SizedBox(
-                                                          height: 26,
-                                                          width: 26,
-                                                          child: Icon(
-                                                            Icons.edit,
-                                                            size: 26.0,
-                                                          ),
-                                                        ),
-                                                      )),
-                                                ]),
-                                            body: ApartmentViewer(
-                                              apartmentLoaded: true,
-                                              currentApartment:
-                                              ownedApartments![index],
-                                            ))));
-                              },
-                            );
-                          }),
-            );
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => Scaffold(
+                                                appBar: AppBar(
+                                                    title: Text(
+                                                        ownedApartments![index]
+                                                            .listingTitle),
+                                                    backgroundColor:
+                                                        Colors.brown,
+                                                    actions: <Widget>[
+                                                      Padding(
+                                                          padding:
+                                                              const EdgeInsets
+                                                                      .only(
+                                                                  right: 20.0),
+                                                          child:
+                                                              GestureDetector(
+                                                            onTap: () {
+                                                              Navigator.push(
+                                                                context,
+                                                                MaterialPageRoute(
+                                                                    builder:
+                                                                        (context) =>
+                                                                            AddApartment(
+                                                                              toEdit: ownedApartments![index],
+                                                                            )),
+                                                              );
+                                                            },
+                                                            child:
+                                                                const SizedBox(
+                                                              height: 26,
+                                                              width: 26,
+                                                              child: Icon(
+                                                                Icons.edit,
+                                                                size: 26.0,
+                                                              ),
+                                                            ),
+                                                          )),
+                                                    ]),
+                                                body: ApartmentViewer(
+                                                  apartmentLoaded: true,
+                                                  currentApartment:
+                                                      ownedApartments![index],
+                                                ))));
+                                  },
+                                );
+                              }),
+                  inAsyncCall: _isLoading,
+                  // demo of some additional parameters
+                  opacity: 0.5,
+                  progressIndicator: const CircularProgressIndicator(),
+                ));
           });
     });
   }

6dfe8d2d4c628778e68456b3ca7751bc5ec14acd
Francesco Ratti <francesco2.ratti@mail.polimi.it>
2022-07-14 14:20:58 +0200

    login with google in a single page

--- lib/pages/loginorsignup.dart
+++ lib/pages/loginorsignup.dart
@@ -1,14 +1,62 @@
-import 'dart:ui';
-
-import 'package:appartapp/pages/google_sign_in_button.dart';
+import 'package:appartapp/classes/connection_exception.dart';
+import 'package:appartapp/classes/enum_loginresult.dart';
+import 'package:appartapp/classes/runtime_store.dart';
+import 'package:appartapp/classes/user.dart' as app_user;
 import 'package:appartapp/utils/authentication.dart';
+import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fb;
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginOrSignup extends StatefulWidget {
+  final String urlStr =
+      "http://ratti.dynv6.net/appartapp-1.0-SNAPSHOT/api/login";
+
+  const LoginOrSignup({Key? key}) : super(key: key);
+
   @override
   _LoginOrSignupState createState() => _LoginOrSignupState();
+
+  Future<List> signIn(fb.User user, String accessToken) async {
+    String idToken = await user.getIdToken();
+    var dio = RuntimeStore().dio; //ok
+    try {
+      Response response = await dio.post(
+        urlStr,
+        data: {"idtoken": idToken, "accesstoken": accessToken},
+        options: Options(
+          contentType: Headers.formUrlEncodedContentType,
+          headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        ),
+      );
+
+      if (response.statusCode != 200) {
+        if (response.statusCode == 401) {
+          return [null, LoginResult.wrong_credentials];
+        } else {
+          return [null, LoginResult.server_error];
+        }
+      } else {
+        Map responseMap = response.data;
+        app_user.User user = app_user.User.fromMap(responseMap);
+
+        return [user, LoginResult.ok];
+      }
+    } on DioError catch (e) {
+      if (e.type == DioErrorType.connectTimeout ||
+          e.type == DioErrorType.receiveTimeout ||
+          e.type == DioErrorType.other ||
+          e.type == DioErrorType.sendTimeout ||
+          e.type == DioErrorType.cancel) {
+        throw ConnectionException();
+      }
+      if (e.response?.statusCode == 401) {
+        return [null, LoginResult.wrong_credentials];
+      }
+      return [null, LoginResult.server_error];
+    }
+  }
 }
 
 class _LoginOrSignupState extends State<LoginOrSignup> {
@@ -60,13 +108,80 @@ class _LoginOrSignupState extends State<LoginOrSignup> {
                         );
                       } else if (snapshot.connectionState ==
                           ConnectionState.done) {
-                        return GoogleSignInButton(
-                          isLoadingCbk: (bool isLoading) {
+                        return SignInButton(
+                          Buttons.Google,
+                          text: "Accedi con Google",
+                          onPressed: () async {
                             setState(() {
-                              _isLoading = isLoading;
+                              _isLoading = true;
                             });
+/*
+
+                            await Future.delayed(Duration(seconds: 1));
+
+                            List? resG = await Authentication.signInWithGoogle(context: context);
+
+                            setState(() {
+                              _isLoading=true;
+                            });
+
+                            if (resG != null) {
+                              fb.User? gUser = resG[0];
+                              String accessToken = resG[1];
+
+                              try {
+                                List res = await widget.signIn(gUser!, accessToken);
+                                LoginResult loginResult = res[1];
+                                switch (loginResult) {
+                                  case LoginResult.ok:
+                                    app_user.User appUser = res[0];
+                                    RuntimeStore()
+                                        .getSharedPreferences()
+                                        ?.setBool("credentialslogin", false);
+                                    RuntimeStore().setCredentialsLogin(false);
+                                    doInitialisation(
+                                        context,
+                                        appUser,
+                                        RuntimeStore().getSharedPreferences()
+                                        as SharedPreferences);
+                                    break;
+                                  case LoginResult.network_fail:
+                                    setState(() {
+                                      _isLoading=false;
+                                    });
+                                    Navigator.restorablePush(
+                                        context, ErrorDialogBuilder.buildConnectionErrorRoute);
+                                    break;
+                                  case LoginResult.wrong_credentials:
+                                    setState(() {
+                                      _isLoading=false;
+                                    });
+                                    Navigator.restorablePush(
+                                        context, ErrorDialogBuilder.buildCredentialsErrorRoute);
+                                    break;
+                                  case LoginResult.server_error:
+                                    setState(() {
+                                      _isLoading=false;
+                                    });
+                                    Navigator.restorablePush(context,
+                                        ErrorDialogBuilder.buildGenericConnectionErrorRoute);
+                                    break;
+                                }
+                              } on ConnectionException {
+                                setState(() {
+                                  _isLoading=false;
+                                });
+                                Navigator.restorablePush(
+                                    context, ErrorDialogBuilder.buildConnectionErrorRoute);
+                              }
+                            } else {
+                              setState(() {
+                                _isLoading=false;
+                              });
+                              Navigator.restorablePush(
+                                  context, ErrorDialogBuilder.buildGenericConnectionErrorRoute);
+                            }*/
                           },
-                          parentContext: context,
                         );
                       }
                       return const CircularProgressIndicator(

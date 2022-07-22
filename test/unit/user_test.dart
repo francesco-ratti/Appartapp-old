4f31a32f4b348d4ff8db7ebf3bc4fc15cb32a116
rocamario <72744822+rocamario@users.noreply.github.com>
2022-07-22 10:57:13 +0200

    test folder reorganized

--- test/unit/user_test.dart
+++ test/unit/user_test.dart
@@ -1,10 +1,10 @@
 import 'package:appartapp/entities/user.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:mockito/annotations.dart';
+//import 'package:mockito/mockito.dart';
+//import 'package:mockito/annotations.dart';
 //import 'user.mocks.dart';
 
-@GenerateMocks([User])
+//@GenerateMocks([User])
 void main() {
   group('Testing user...', () {
     test('Checking a complete profile', () {
@@ -116,6 +116,29 @@ void main() {
       User usr = User.fromMap(mapUsr);
 
       expect(usr.isProfileComplete(), false);
+    });
+
+    test('Checking a complete profile without pets', () {
+      var mapUsr = {
+        'id': 000,
+        "email": 'email@email.it',
+        'name': 'Mario',
+        'surname': 'Roca',
+        'birthday': 919551600000,
+        'gender': 'M',
+        'images': [],
+        'bio': "text",
+        'reason': 'study',
+        'month': 'Settembre',
+        'job': 'student',
+        'income': '1600',
+        'smoker': 'No',
+        'pets': ''
+      };
+      User usr = User.fromMap(mapUsr);
+
+      expect(usr.isProfileComplete(), true);
+      expect(usr.hasPets(), false);
     });
   });
 }
